8e9238a5e7ecc394e399d24dcfa582c44588189d
ra2255685 <raren003@ucr.edu>
2019-01-25 17:03:17 -0800

    Lab 5 part 2: updated Output to be on PORTB and variable names to match the change.

--- raren003_lab5_part2/raren003_lab5_part2/main.c
+++ raren003_lab5_part2/raren003_lab5_part2/main.c
@@ -11,7 +11,7 @@
 
 enum States {Start, INIT, WAIT, INCREMENT, DECREMENT, HOLD} state;
 
-unsigned char tmpC = 0x00;
+unsigned char tmpB = 0x00; //counter variable for setting portB
 
 void Tick(){
 	
@@ -62,27 +62,27 @@ void Tick(){
 	
 	switch(state){				//state actions
 		case INIT:
-		tmpC = 0;
-		PORTC = tmpC;
+		tmpB = 0;
+		PORTB = tmpB;
 		break;
 		
 		case WAIT:
 		break;
 		
 		case INCREMENT:
-		if (tmpC < 0x09)
+		if (tmpB < 0x09)
 		{
-			tmpC = tmpC + 1;
+			tmpB = tmpB + 1;
 		}
-		PORTC = tmpC;
+		PORTB = tmpB;
 		break;
 		
 		case DECREMENT:
-		if (tmpC > 0x00)
+		if (tmpB > 0x00)
 		{
-			tmpC = tmpC - 1;
+			tmpB = tmpB - 1;
 		}
-		PORTC = tmpC;
+		PORTB = tmpB;
 		break;
 		
 		case HOLD:
@@ -96,7 +96,7 @@ void Tick(){
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;	// Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00;	// Configure port B's 8 pins as outputs, Initialize 0’s
+	DDRB = 0xFF; PORTB = 0x00;	// Configure port B's 8 pins as outputs, Initialize 0’s
 	
 	state = Start;	//initial call
 	
