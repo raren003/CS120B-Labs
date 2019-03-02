b69e0df3e47f11830319292b42610a67035a24b6
ra2255685 <raren003@ucr.edu>
2019-03-02 12:47:12 -0800

    Lab 7: pre-lab edtied code to expriment with lcd

--- raren003_lab7_part0/raren003_lab7_part0/main.c
+++ raren003_lab7_part0/raren003_lab7_part0/main.c
@@ -9,6 +9,7 @@
 
 #include <avr/io.h>
 #include "io.c" 
+#include "timer.h"
 
 
 int main(void)
@@ -16,17 +17,32 @@ int main(void)
     DDRC = 0xFF; PORTC = 0x00;	//LCD data lines
 	DDRD = 0xFF; PORTD = 0x00;	//LCD control lines
 	
-	unsigned char* LCD_Output = "Hello World";
+	unsigned char* LCD_Output = " CS120B is Legend... wait for it DARY!";
+	unsigned char i = 0;
 	
 	//initialize the LCD display
 	LCD_init();
 	
+	TimerSet(350);
+	TimerOn();
+	
 	//starting at position 1 on the LCD screen, writes Hello World
-	LCD_DisplayString(1, LCD_Output);
+	//LCD_DisplayString(1, LCD_Output);
+	
+	LCD_Cursor(1);
 	
     while (1) 
     {
-		continue;
-    }
+		//LCD_DisplayString(1, LCD_Output);
+		
+		for (i=32; i>0; i--)
+		{
+			LCD_DisplayString(i, LCD_Output);
+			while (!TimerFlag);
+			TimerFlag = 0;
+			
+		}
+		i = 32;
+	 }
 }
 
