2e12f445d6cc2d6182ec4f0dd162e37f8dcdccf5
ra2255685 <raren003@ucr.edu>
2019-03-07 21:53:33 -0800

    Programs used for learning how to use two ATmega1284's

---
+++ raren003_USARTTesting/raren003_USARTTesting/main.c
@@ -0,0 +1,164 @@
+/* Partner(s) Name & E-mail: Robert Arenas, raren003@ucr.edu
+* Lab Section: 022
+* Assignment: experimenting with usart
+* Exercise Description:
+*
+* I acknowledge all content contained herein, excluding template or example
+* code, is my own original work.
+*/
+
+#include <avr/io.h>
+#include <avr/common.h>
+#include <avr/interrupt.h>
+#include "usart.h"
+
+
+typedef struct task {
+	int state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*TickFct)(int);
+} task;
+
+task tasks[1];
+const unsigned short tasksNum = 1;
+const unsigned long taskPeriodGCD = 1000;
+const unsigned long taskPeriodFollow = 1000;
+
+
+unsigned char temp;
+
+enum Follower_STATES {FOLW_start, FOLW_ON};
+int TickFct_FOLLOWER(int state){
+	
+	switch(state){			//Transitions
+		
+		case FOLW_start:
+			state = FOLW_ON;
+			break;
+			
+		case  FOLW_ON:
+			state = FOLW_ON;
+			break;
+			
+		default:
+			break;
+		
+	}						//Transitions
+	
+	switch(state){			//State actions
+		
+		case FOLW_start:
+		break;
+		
+		case  FOLW_ON:
+			if (USART_HasReceived(0)){
+				temp = USART_Receive(0);
+				PORTB = temp;
+				USART_Flush(0);
+			}
+		break;
+		
+		default:
+		break;
+		
+	}						//State Actions
+	
+	return state;
+}
+
+///////////////////////////////////////////////////////////////
+////////////FUNCTIONS FOR THE TIMER////////////////////////////
+///////////////////////////////////////////////////////////////
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B 	= 0x0B;	// bit3 = 1: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: prescaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A 	= 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+
+	TIMSK1 	= 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	//Enable global interrupts
+	SREG |= 0x80;	// 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B 	= 0x00; // bit3bit2bit1bit0=0000: timer off
+}
+
+/*void TimerISR() {
+	TimerFlag = 1;
+}*/
+
+void TimerISR(){
+	unsigned char i;
+	for (i = 0; i <tasksNum; ++i){
+		if ( tasks[i].elapsedTime >= tasks[i].period){
+			tasks[i].state = tasks[i].TickFct(tasks[i].state);
+			tasks[i].elapsedTime = 0;
+		}
+		tasks[i].elapsedTime += taskPeriodGCD;
+	}
+	//TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect)
+{
+	// CPU automatically calls when TCNT0 == OCR0 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; 			// Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { 	// results in a more efficient compare
+		TimerISR(); 				// Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+
+int main(void)
+{
+	DDRB = 0xFF; PORTB = 0x00; //set B as output
+	
+	initUSART(0);
+	
+	unsigned char i = 0;
+	tasks[i].state = FOLW_start;
+	tasks[i].period = taskPeriodFollow;
+	tasks[i].elapsedTime = 0;
+	tasks[i].TickFct = &TickFct_FOLLOWER;
+	
+	TimerSet(taskPeriodGCD);
+	TimerOn();
+    
+    while (1) 
+    {
+    }
+}
+
---
+++ raren003_USARTTesting2/raren003_USARTTesting2/main.c
@@ -0,0 +1,177 @@
+/* Partner(s) Name & E-mail: Robert Arenas, raren003@ucr.edu
+* Lab Section: 022
+* Assignment: experimenting with usart
+* Exercise Description:
+*
+* I acknowledge all content contained herein, excluding template or example
+* code, is my own original work.
+*/
+
+#include <avr/io.h>
+#include <avr/common.h>
+#include <avr/interrupt.h>
+#include "usart.h"
+#include "shiftreg_write.h"
+
+
+typedef struct task {
+	int state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*TickFct)(int);
+} task;
+
+task tasks[1];
+const unsigned short tasksNum = 1;
+const unsigned long taskPeriodGCD = 1000;
+const unsigned long taskPeriodFollow = 1000;
+
+
+unsigned char temp;
+
+enum Leader_STATES {leadr_start, leadr_ON, leadr_OFF};
+int TickFct_Leader(int state){
+	
+	switch(state){			//Transitions
+		
+		case leadr_start:
+			state = leadr_ON;
+			break;
+			
+		case  leadr_ON:
+			state = leadr_OFF;
+			break;
+			
+		case leadr_OFF:
+			state = leadr_ON;
+			break;
+			
+		default:
+			break;
+		
+	}						//Transitions
+	
+	switch(state){			//State actions
+		
+		case leadr_start:
+		break;
+		
+		case  leadr_ON:
+			if(USART_IsSendReady(0)){
+				USART_Send(0x01, 0);
+				transmit_data(0x01);
+				USART_Flush(0);
+			}
+			break;
+		
+		case leadr_OFF:
+			if(USART_IsSendReady(0)){
+				USART_Send(0x00, 0);
+				transmit_data(0x00);
+				USART_Flush(0);
+			}
+			break;
+		
+		default:
+		break;
+		
+	}						//State Actions
+	
+	return state;
+}
+
+///////////////////////////////////////////////////////////////
+////////////FUNCTIONS FOR THE TIMER////////////////////////////
+///////////////////////////////////////////////////////////////
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B 	= 0x0B;	// bit3 = 1: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: prescaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A 	= 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+
+	TIMSK1 	= 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	//Enable global interrupts
+	SREG |= 0x80;	// 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B 	= 0x00; // bit3bit2bit1bit0=0000: timer off
+}
+
+/*void TimerISR() {
+	TimerFlag = 1;
+}*/
+
+void TimerISR(){
+	unsigned char i;
+	for (i = 0; i <tasksNum; ++i){
+		if ( tasks[i].elapsedTime >= tasks[i].period){
+			tasks[i].state = tasks[i].TickFct(tasks[i].state);
+			tasks[i].elapsedTime = 0;
+		}
+		tasks[i].elapsedTime += taskPeriodGCD;
+	}
+	//TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect)
+{
+	// CPU automatically calls when TCNT0 == OCR0 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; 			// Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { 	// results in a more efficient compare
+		TimerISR(); 				// Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+
+int main(void)
+{
+	DDRB = 0xFF; PORTB = 0x00; //set B as output
+	
+	initUSART(0);
+	
+	unsigned char i = 0;
+	tasks[i].state = leadr_start;
+	tasks[i].period = taskPeriodFollow;
+	tasks[i].elapsedTime = 0;
+	tasks[i].TickFct = &TickFct_Leader;
+	
+	TimerSet(taskPeriodGCD);
+	TimerOn();
+    
+    while (1) 
+    {
+    }
+}
+
