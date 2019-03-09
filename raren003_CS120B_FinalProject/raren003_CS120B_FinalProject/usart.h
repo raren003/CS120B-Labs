67e3a8e60dd0523b97e47322594905dd1e8c8bde
ra2255685 <raren003@ucr.edu>
2019-03-08 21:04:17 -0800

    Final Project: added working usart.h file

--- raren003_CS120B_FinalProject/raren003_CS120B_FinalProject/usart.h
+++ raren003_CS120B_FinalProject/raren003_CS120B_FinalProject/usart.h
@@ -1,11 +1,11 @@
+
 // Permission to copy is granted provided that this header remains intact.
 // This software is provided with no warranties.
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
-#ifndef USART_H_
-#define USART_H_
+#ifndef USART_H
+#define USART_H
 
 // USART Setup Values
 #define F_CPU 8000000UL // Assume uC operates at 8MHz
@@ -14,105 +14,73 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //Functionality - Initializes TX and RX on PORT D
-//Parameter: usartNum specifies which USART is being initialized
-//			 If usartNum != 1, default to USART0
+//Parameter: None
 //Returns: None
-void initUSART(unsigned char usartNum)
+void initUSART()
 {
-	if (usartNum != 1) {
-		// Turn on the reception circuitry of USART0
-		// Turn on receiver and transmitter
-		// Use 8-bit character sizes
-		UCSR0B |= (1 << RXEN0)  | (1 << TXEN0);
-		UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
-		// Load lower 8-bits of the baud rate value into the low byte of the UBRR0 register
-		UBRR0L = BAUD_PRESCALE;
-		// Load upper 8-bits of the baud rate value into the high byte of the UBRR0 register
-		UBRR0H = (BAUD_PRESCALE >> 8);
-	}
-	else {
-		// Turn on the reception circuitry for USART1
-		// Turn on receiver and transmitter
-		// Use 8-bit character sizes
-		UCSR1B |= (1 << RXEN1)  | (1 << TXEN1);
-		UCSR1C |= (1 << UCSZ10) | (1 << UCSZ11);
-		// Load lower 8-bits of the baud rate value into the low byte of the UBRR1 register
-		UBRR1L = BAUD_PRESCALE;
-		// Load upper 8-bits of the baud rate value into the high byte of the UBRR1 register
-		UBRR1H = (BAUD_PRESCALE >> 8);
-	}
+	// Turn on the reception circuitry
+	// Use 8-bit character sizes - URSEL bit set to select the UCRSC register
+	// Turn on receiver and transmitter
+	UCSR0B |= (1 << RXEN0)  | (1 << TXEN0);
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
+	// Load lower 8-bits of the baud rate value into the low byte of the UBRR register
+	UBRR0L = BAUD_PRESCALE;
+	// Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+	UBRR0H = (BAUD_PRESCALE >> 8);
 }
 ////////////////////////////////////////////////////////////////////////////////
 //Functionality - checks if USART is ready to send
-//Parameter: usartNum specifies which USART is checked
+//Parameter: None
 //Returns: 1 if true else 0
-unsigned char USART_IsSendReady(unsigned char usartNum)
+unsigned char USART_IsSendReady()
 {
-	return (usartNum != 1) ? (UCSR0A & (1 << UDRE0)) : (UCSR1A & (1 << UDRE1));
+	return (UCSR0A & (1 << UDRE0));
 }
 ////////////////////////////////////////////////////////////////////////////////
-//Functionality - checks if USART has successfully transmitted data
-//Parameter: usartNum specifies which USART is being checked
+//Functionality - checks if USART has recieved data
+//Parameter: None
 //Returns: 1 if true else 0
-unsigned char USART_HasTransmitted(unsigned char usartNum)
+unsigned char USART_HasTransmitted()
 {
-	return (usartNum != 1) ? (UCSR0A & (1 << TXC0)) : (UCSR1A & (1 << TXC1));
+	return (UCSR0A & (1 << TXC0));
 }
 ////////////////////////////////////////////////////////////////////////////////
 // **** WARNING: THIS FUNCTION BLOCKS MULTI-TASKING; USE WITH CAUTION!!! ****
 //Functionality - checks if USART has recieved data
-//Parameter: usartNum specifies which USART is checked
+//Parameter: None
 //Returns: 1 if true else 0
-unsigned char USART_HasReceived(unsigned char usartNum)
+unsigned char USART_HasReceived()
 {
-	return (usartNum != 1) ? (UCSR0A & (1 << RXC0)) : (UCSR1A & (1 << RXC1));
+	return (UCSR0A & (1 << RXC0));
 }
 ////////////////////////////////////////////////////////////////////////////////
 //Functionality - Flushes the data register
-//Parameter: usartNum specifies which USART is flushed
+//Parameter: None
 //Returns: None
-void USART_Flush(unsigned char usartNum)
+void USART_Flush()
 {
 	static unsigned char dummy;
-	if (usartNum != 1) {
-		while ( UCSR0A & (1 << RXC0) ) { dummy = UDR0; }
-	}
-	else {
-		while ( UCSR1A & (1 << RXC1) ) { dummy = UDR1; }
-	}
+	while ( UCSR0A & (1 << RXC0) ) { dummy = UDR0; }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // **** WARNING: THIS FUNCTION BLOCKS MULTI-TASKING; USE WITH CAUTION!!! ****
 //Functionality - Sends an 8-bit char value
 //Parameter: Takes a single unsigned char value
-//			 usartNum specifies which USART will send the char
 //Returns: None
-void USART_Send(unsigned char sendMe, unsigned char usartNum)
+void USART_Send(unsigned char sendMe)
 {
-	if (usartNum != 1) {
-		while( !(UCSR0A & (1 << UDRE0)) );
-		UDR0 = sendMe;
-	}
-	else {
-		while( !(UCSR1A & (1 << UDRE1)) );
-		UDR1 = sendMe;
-	}
+	while( !(UCSR0A & (1 << UDRE0)) );
+	UDR0 = sendMe;
 }
 ////////////////////////////////////////////////////////////////////////////////
 // **** WARNING: THIS FUNCTION BLOCKS MULTI-TASKING; USE WITH CAUTION!!! ****
 //Functionality - receives an 8-bit char value
-//Parameter: usartNum specifies which USART is waiting to receive data
+//Parameter: None
 //Returns: Unsigned char data from the receive buffer
-unsigned char USART_Receive(unsigned char usartNum)
+unsigned char USART_Receive()
 {
-	if (usartNum != 1) {
-		while ( !(UCSR0A & (1 << RXC0)) ); // Wait for data to be received
-		return UDR0; // Get and return received data from buffer
-	}
-	else {
-		while ( !(UCSR1A & (1 << RXC1)) );
-		return UDR1;
-	}
+	while ( !(UCSR0A & (1 << RXC0)) ); // Wait for data to be received
+	return UDR0; // Get and return received data from buffer
 }
 
-#endif /* USART_H_ */
+#endif //USART_H
